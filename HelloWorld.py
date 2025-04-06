81a2b37f714115cb6c2942ce6c5999626de2605e
Brett Knox <BrettKnox2003@gmai.com>
2025-04-06 15:58:18 -0400

    lets see how this works

---
+++ HelloWorld.py
@@ -0,0 +1,22 @@
+# x,y = 10   #didnt work , might not be able to declare variables this way
+# no datat type specification needed when declaring the variable
+# to print you use the " print" keyword
+# no need for semi-colons to indicate the end of statements; you use enters/ newlines and tabs for such
+ # """  """  functions as a multiline comment, while "#" functions as a single line comment
+
+"""
+x = 10
+y = x*x
+print("hello" + x)
+print("howdy" + y)
+# also didn't work, seems to concatonate differently
+"""
+print("Hello World")
+# basic boolean type cast and if condition
+f = bool(1)
+if(f):
+    print("yipee")
+"""else: if(!f):
+    print("yagga")
+    #doesnt work, !f for not true -> false doesnt seem to work
+"""
